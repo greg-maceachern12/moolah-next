01cd96eea8985f8addf3dca504cab715ddfaddbe
greg-maceachern12 <gregmaceachern98@gmail.com>
2025-01-24 16:19:13 -0500

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next'
 import SpendingDashboard from '@/app/components/SpendingDashboard'
 
 export const metadata: Metadata = {
-  title: 'Moolah Dashboard',
+  title: 'Moolah | Understand Your Spending',
   description: 'Analyze your financial data with ease',
 }
 
